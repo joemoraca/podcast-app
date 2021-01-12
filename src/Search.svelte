e50756c47346f1da58ad88478acfae3909ab29d6
Joe Moraca <joemoraca@gmail.com>
2021-01-11 19:22:56 -0500

    progress on search3

--- src/Search.svelte
+++ src/Search.svelte
@@ -1,28 +1,26 @@
 <script>
     let sresults ='';
-  //  let sresults2 ='';
-    let squery = 'Liberty';
+    let squery = '';
     var options = { year: 'numeric', month: 'long', day: 'numeric' };
     let mindate = Date.now() - 5184000000;
 
     async function getSearchResult() {
         const url = "./pod-get-search-pods.php?squery=" + squery;
-       // const url = "./pod-get-search-pods.php";
         let res = await fetch(url);
         res = await res.json();
         sresults = res.feeds;
-// trying to only show podcasts last updated within 60 days
-    //  console.log(Date.now() - mindate);
-    //  sresults2 =  sresults.filter(newpods => sresults.lastUpdateTime >= 1);
+        console.log(squery);
+// trying to only show podcasts last updated within 60 days then sorted newest first
+    //  sresults =  sresults.filter(newpods => newpods.lastUpdateTime > mindate/1000);
         sresults = sresults.filter(function (e) {return e.lastUpdateTime > mindate/1000;});
         sresults.sort((a, b) => parseFloat(b.lastUpdateTime) - parseFloat(a.lastUpdateTime));
-        console.log(sresults)
+    //    console.log(sresults)
     }
 </script>
 
     <fieldset>
         <label for="">Search Podcast Index Podcasts</label>
-        <input type="text" />
+        <input bind:value={squery} type="text" placeholder="seach by one term">
         <button on:click={() => getSearchResult()}>Search</button>
     </fieldset>
 <table width="100%">
